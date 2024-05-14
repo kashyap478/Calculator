4cb8f1f4c2f91079942b2b7fe5204680e43b2e3f
janvi_ksyp <144678746+kashyap478@users.noreply.github.com>
2024-05-14 22:15:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let string = "";
+let input =document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+Array.from(buttons).forEach(button =>{
+    button.addEventListener('click',(e) =>{
+       if(e.target.innerHTML == '='){
+        string = eval(string);
+        input.value = string;
+       }
+       else if(e.target.innerHTML == 'AC'){
+            string = "" ;
+            input.value = string;
+          }
+          else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0,string.length-1);
+            input.value = string;
+          }
+          else{
+       string = string + e.target.innerHTML;
+       input.value = string;}
+    })
+})
